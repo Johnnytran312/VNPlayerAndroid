5a3dd2f1b9aaa29284e3ad70a81f82c9a8cf8c28
nnbinh <nnbinh@mytrax.co.jp>
2017-01-12 17:02:04 +0700

    rm image, blank code

--- app/src/main/java/android/nnbinh/hcmus/vietnam_player/MainActivity.java
+++ app/src/main/java/android/nnbinh/hcmus/vietnam_player/MainActivity.java
@@ -1,38 +1,21 @@
 package android.nnbinh.hcmus.vietnam_player;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.nnbinh.hcmus.vietnam_player.Custom.CustomTab;
-import android.nnbinh.hcmus.vietnam_player.Custom.CustomTabLayout;
 import android.nnbinh.hcmus.vietnam_player.activity.BaseActivity;
-import android.nnbinh.hcmus.vietnam_player.activity.EqualizerActivity;
-import android.nnbinh.hcmus.vietnam_player.activity.ThemeActivity;
-import android.nnbinh.hcmus.vietnam_player.adapter.MyAdapter;
-import android.nnbinh.hcmus.vietnam_player.adapter.MyPagerAdapter;
 import android.nnbinh.hcmus.vietnam_player.fragment.EqualizerFragment;
 import android.nnbinh.hcmus.vietnam_player.fragment.MainFragment;
 import android.nnbinh.hcmus.vietnam_player.fragment.ThemeFragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     public Toolbar getToolbar() {
